dd8b8d4feb2a2f1111837994d2336a0103fa0b38
OE10x2 <OE10x2@gmail.com>
2019-11-10 20:09:26 -0500

    Final

--- src/IntegerSorter.java
+++ src/IntegerSorter.java
@@ -1,38 +1,104 @@
+/*
+Name: Ethan Zhang
+Course: ICS4U1-01
+Teacher: Mr. Radulovic
+Date finished: November 4, 2019
+Assignment: Efficiency of Sorting Algorithms
+
+The following program simulates 3 different types of sorting algorithms:
+- Bubble Sort (Method 1)
+- Reverse Bubble Sort (Method 2)
+- Merge Sort (Method 3)
+After sorting the given list (read from a file), the program outputs the amount of time
+it took for the sort to finish. The given list has a size between 2^4 and 2^23, inclusive.
+*/
+
 import java.io.File;
 import java.util.Scanner;
 
 public class IntegerSorter implements Sorter{
 
-    private int[] list, tempList;
+    private int[] list; //Used for sort methods
+
+    public static void main(String[] args) throws Exception{
+        int sortNum = 1; //CHANGE SORT METHOD HERE
+        File file = new File("2power5.txt");
+        Scanner SCAN = new Scanner(file);
+        int values = 0; //Used to find size of the file
+        while (SCAN.hasNextInt()){
+            SCAN.nextInt(); //Keep on reading integers from the file until none are left
+            values++;
+        }
+        int[] getList = new int[values]; //Reserve a list for values from input
+        SCAN.close(); //Reset the scanner
+        SCAN = new Scanner(file); //Pointer moves back to beginning
+        int temp = 0;
+        while (SCAN.hasNext()) getList[temp++] = SCAN.nextInt(); //Store the values
+        IntegerSorter IS = new IntegerSorter(); //Used to call the methods for sorting
+        IS.setList(getList); //Assign the values
+
+        long start = System.nanoTime();
+        IS.sort(sortNum);
+        long end = System.nanoTime();
+        System.out.println("TIME: " + (end - start) / 1e9 + " seconds");
+        //Shows the time it took to compile (in seconds)
+        //System.out.println(IS.toString());
+    }
+
+    //This method is used to swap two elements, given their indices
+    private void swap(int a, int b){
+        int temp = list[a];
+        list[a] = list[b];
+        list[b] = temp;
+    }
 
     private void sort_method1(){
-        for (int i = list.length - 1; i >= 0; i--){
+        /*
+        General idea of this sort method:
+        For every loop, compare the consecutive elements, and place the smaller element in front of the
+        larger one. After one loop, the last element is guaranteed to be the largest element. As a result,
+        we only loop through the n-1 elements for the next loop, and so on. The complexity of this program
+        is around O(N^2), since we have a nested for loop, with each looping through the entire array.
+         */
+        for (int i = list.length - 1; i > 0; i--){
+            //"i" represents the number of times we loop through the array
             for (int j = 0; j < i; j++){
-                if (list[j] > list[j+1]){
-                    int temp = list[j];
-                    list[j] = list[j+1];
-                    list[j+1] = temp;
-                }
+                //"j" represents the index of the element
+                if (list[j] > list[j + 1]) swap(j, j + 1); //Swap if not in order
             }
         }
     }
 
     private void sort_method2(){
+        /*
+        General idea for this method:
+        For each loop, we always compare the (constantly updated) first element with the next element,
+        which is looped through as well. We compare each pair of elements, and we place the smaller
+        element in the first slot. As a result, after each loop, the first element is guaranteed to
+        be the smallest element of the entire list. For the second loop, the second element is the
+        second smallest element, and so on. The complexity is also O(N^2).
+         */
         for (int i = 0; i < list.length; i++){
+            //"i" represents the first index
             for (int j = i + 1; j < list.length; j++){
-                if (list[i] > list[j]){
-                    int temp = list[j];
-                    list[j] = list[i];
-                    list[i] = temp;
-                }
+                //"j" represents the second index
+                if (list[i] > list[j]) swap(i, j); //Swap if not in order
             }
         }
     }
 
     private void sort_method3(int start, int end){
+        /*
+        General idea of this method:
+        Split the entire array into smaller pieces, preferably splitting into half each time.
+        After that, first sort each piece, then join them together. The complexity of this
+        method is O(N log N), since splitting the entire array would result in log N sub-arrays.
+        (log here means log base 2)
+         */
         if (end - start > 1){
-            int middle = (start + end) / 2;
-            sort_method3(start, middle); //Break down to two sections
+            //The size of the subarray passed in is still larger than 1
+            int middle = (start + end) / 2; //Break down to two sections
+            sort_method3(start, middle);
             sort_method3(middle, end);
             combineList(start, end);
             //After calling sort_method3 for both sections, we know that each section is already sorted.
@@ -43,11 +109,13 @@ public class IntegerSorter implements Sorter{
     private void combineList(int start, int end){
         int length = end - start, middle = (start + end) / 2;
         int pointX = start, pointY = middle, cnt = 0;
-        tempList = new int[length]; //Reset the array
+        int[] tempList = new int[length]; //Reset the array
         while (pointX < middle && pointY < end){
+            //Not all the elements are added to the sorted array
             if (list[pointX] <= list[pointY]) tempList[cnt++] = list[pointX++];
             else tempList[cnt++] = list[pointY++];
-
+            //Takes the smaller element and adds it to the array
+            //At the same time, update the array indices
         }
         //Clear up the leftover values
         while (pointX < middle) tempList[cnt++] = list[pointX++];
@@ -78,20 +146,5 @@ public class IntegerSorter implements Sorter{
         String ans = "";
         for (int i: list) ans += i + " ";
         return ans;
-    }
-
-    public static void main(String[] args) throws Exception{
-        int size = new Scanner(System.in).nextInt();
-        if (size < 4 || size > 21) return;
-        int[] list = new int[(int)Math.pow(2, size)];
-        Scanner SCAN = new Scanner(new File("2power"+ size + ".txt"));
-        int temp = 0;
-        while (SCAN.hasNext()) list[temp++] = SCAN.nextInt();
-        IntegerSorter IS = new IntegerSorter();
-        IS.setList(list);
-        long start = System.nanoTime();
-        IS.sort(2);
-        long end = System.nanoTime();
-        System.out.println("TIME: " + (end - start) / 1e9 + " seconds");
     }
 }
